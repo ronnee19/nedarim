09547d725d97e62c3f1390eeaa025f9057582ca1
ronnee19 <122403782+ronstip@users.noreply.github.com>
2023-10-10 18:34:25 +0300

    add missing

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import pandas as pd
 import streamlit as st
 from funcs.utils import get_unified_data
 
+st.set_page_config(layout="wide")
 
-# cols = ['שם', 'שם ביניים'] 
+
+st.header('טבלת נעדרים')
+
 
 # data = get_unified_data()
 
@@ -14,9 +17,16 @@ data = pd.read_csv('data/data.csv', encoding='utf-8')
 
 # שם	שם ביניים	שם משפחה	ת.ז.	מס' דרכון	מין	גיל	אזרח/ית / חייל/ת	תמונה	צבע עיניים	צבע שיער	גובה	מבנה גוף	לבוש	סימנים על הגוף	טלפון 1 של הנעדר/ת	טלפון 2 של הנעדר/ת	מספר של מישהו נוסף שנמצא איתו	כתובת מייל של הנעדר/ת	כתובת מגורים של הנעדר/ת	שעת קשר אחרונה	מיקום אחרון	סטטוס מקורי	מקור מידע סטטוס מקורי	מופיע באתר "נעדרים"	מצב רפואי	שם המדווח	טלפון של המדווח/ת	כתובת מייל של המדווח/ת	קרבת המדווח/ת לנעדר/ת	סטטוס עדכני	מקור מידע סטטוס עדכני	תאריך עדכון אחרון	יצר קשר עם משפחה/חברים	מקור מידע	פייסבוק	אינסטגראם	טוויטר	טיקטוק	קונפליקט במידע	הערות										
 # use the abot to create a table columns
-columns = ['שם', 'שם ביניים'] 
+if st.checkbox('Show all columns'):
+    cols = data.columns
+else:
+    cols = ['שם', 'שם משפחה', 'ת.ז.', 'מין', 'גיל', 'אזרח/ית / חייל/ת', 'טלפון 1 של הנעדר/ת', 'שם המדווח/ת 1', 'טלפון של המדווח/ת 1']
 
-st.header('Needarim app')
-st.table(data)
+
+st.table(data[cols])
+
+# download csv button
+
+
 # st.line_chart(data)
 
--- pages/add missing person.py
+++ pages/add missing person.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
 st.title('add person')
+
+st.text_input('שם פרטי')
+st.text_input('שם משפחה')
