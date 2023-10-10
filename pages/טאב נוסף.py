3b8709ee338c0ec81281a94f26f17926851ed023
ronnee19 <122403782+ronstip@users.noreply.github.com>
2023-10-11 02:59:08 +0300

    upgrade st

---
+++ pages/טאב נוסף.py
@@ -0,0 +1,13 @@
+# streamlit_app.py
+
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object.
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
