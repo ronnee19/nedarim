1c89450f408f1ba32959b9fcbfee8c2b2b6cd6fe
ronnee19 <122403782+ronstip@users.noreply.github.com>
2023-10-11 02:24:30 +0300

    edit_row_1

--- pages/0_מילוי מידע נעדר קיים.py
+++ pages/0_מילוי מידע נעדר קיים.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+from funcs.utils import upload_dataframe_to_drive
+
 
 st.markdown("""
 <style>
@@ -19,31 +21,59 @@ import pandas as pd
 
 st.title('מילוי מידע נעדר קיים')
 
-c2, c1 = st.columns(2)
-
-full_name = c2.selectbox(label= 'בחר שם', options=st.session_state['uni_data']["שם מלא"])
+a1, a2 = st.columns([1, 1])
+full_name = a1.selectbox(label= 'בחר שם', options=st.session_state['uni_data']["שם מלא"])
 idx = st.session_state['uni_data'][st.session_state['uni_data']['שם מלא'] == full_name].index[0]
+# a2.button('שמור פרטים', key='save_btn')
+st.markdown("--------")
 
-c2, z, c1 = st.columns([5,1,5])
+c2, c1 = st.columns([2,3])
 with c2:
-    for col in ['מין', 'גיל', 'סטטוס עדכני', 'מיקום אחרון', 'טלפון 1 של הנעדר/ת', 'שם באנגלית', 'מקור מידע']:
-        a1, a2 = st.columns([1, 3])
-        a2.text_input(label=col, value=st.session_state['uni_data'].loc[idx, col], key=col + '_input')
-        if a1.button(label='עדכן', key=col):
-            st.session_state['uni_data'].loc[idx, col] = st.session_state[col + '_input']
-            # save the data to csv file
-            st.session_state['uni_data'].to_csv('data/unified.csv', encoding='utf-8', index=False)
+    # Input widgets for editing
+    new_data = {'מין': st.text_input('מין', value=st.session_state['uni_data'].loc[idx, 'מין']),
+                'גיל': st.text_input('גיל', value=st.session_state['uni_data'].loc[idx, 'גיל']),
+                'סטטוס עדכני': st.text_input('סטטוס עדכני', value=st.session_state['uni_data'].loc[idx, 'סטטוס עדכני']),
+                'מיקום אחרון': st.text_input('מיקום אחרון', value=st.session_state['uni_data'].loc[idx, 'מיקום אחרון']),
+                'טלפון 1 של הנעדר/ת': st.text_input('טלפון 1 של הנעדר/ת', value=st.session_state['uni_data'].loc[idx, 'טלפון 1 של הנעדר/ת']),
+                'שם באנגלית': st.text_input('שם באנגלית', value=st.session_state['uni_data'].loc[idx, 'שם באנגלית']),
+                'מקור מידע': st.text_input('מקור מידע', value=st.session_state['uni_data'].loc[idx, 'מקור מידע']),
+    }
+with c1:
+    new_data_to_append = {'אינסטגראם': st.text_input('אינסטגראם', value=st.session_state['uni_data'].loc[idx, 'אינסטגראם']),
+                          'טיקטוק': st.text_input('טיקטוק', value=st.session_state['uni_data'].loc[idx, 'טיקטוק']),
+                          'פייסבוק': st.text_input('פייסבוק', value=st.session_state['uni_data'].loc[idx, 'פייסבוק']),
+    }
+if a2.button('שמור פרטים', key='save_btn'):
+    for col in new_data:
+        st.session_state['uni_data'].loc[idx, col] = new_data[col]
+    for col in new_data_to_append:
+        st.session_state['uni_data'].loc[idx, col] = new_data_to_append[col]
+    # save the data to csv file
+    with st.spinner('מעדכן טבלה...'):
+        upload_dataframe_to_drive(st.session_state['uni_data'])    
+    a2.success('הטבלה נשמרה בהצלחה')
+
+
+# c2, z, c1 = st.columns([5,1,5])
+# with c2:
+#     for col in ['מין', 'גיל', 'סטטוס עדכני', 'מיקום אחרון', 'טלפון 1 של הנעדר/ת', 'שם באנגלית', 'מקור מידע']:
+#         a1, a2 = st.columns([1, 3])
+#         a2.text_input(label=col, value=st.session_state['uni_data'].loc[idx, col], key=col + '_input')
+#         if a1.button(label='עדכן', key=col):
+#             st.session_state['uni_data'].loc[idx, col] = st.session_state[col + '_input']
+#             # save the data to csv file
+#             st.session_state['uni_data'].to_csv('data/unified.csv', encoding='utf-8', index=False)
                     
     
 
-with c1:
-    for col in ['אינסטגראם', 'טיקטוק', 'פייסבוק']:
-        a1, a2 = st.columns([1, 3])
-        a2.text_input(label=col, value=st.session_state['uni_data'].loc[idx, col], key=col + '_input')
-        if a1.button(label='עדכן', key=col):
-            st.session_state['uni_data'].loc[idx, col] = st.session_state[col + '_input']
-            # save the data to csv file
-            st.session_state['uni_data'].to_csv('data/unified.csv', encoding='utf-8', index=False)
+# with c1:
+#     for col in ['אינסטגראם', 'טיקטוק', 'פייסבוק']:
+#         a1, a2 = st.columns([1, 3])
+#         a2.text_input(label=col, value=st.session_state['uni_data'].loc[idx, col], key=col + '_input')
+#         if a1.button(label='עדכן', key=col):
+#             st.session_state['uni_data'].loc[idx, col] = st.session_state[col + '_input']
+#             # save the data to csv file
+#             st.session_state['uni_data'].to_csv('data/unified.csv', encoding='utf-8', index=False)
             
 # add a save button to all the inputs
 # if st.button(label='שמור', key='save'):
