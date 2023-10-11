7c94966246fc06da720f142cca181eb82dec0c93
David Oz <daveoz@meta.com>
2023-10-11 11:41:06 +0300

    Add psql connection (uder turned off flag)

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # nedarim
+
+To switch connecton into postgress database do the following:
+
+Create a file: "[project dir]/.streamlit/secrets.toml"
+put the following in the file:
+
+# .streamlit/secrets.toml
+
+[connections.postgresql]
+dialect = "postgresql"
+host = "localhost"
+port = "5432"
+database = "xxx"
+username = "xxx"
+password = "xxx"
+
+
+get credd/details from David Oz via secured way
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # streamlit run main.py
 # make a dashboard that shows the data set
 
+DB_ON = False
+
 import pandas as pd
 import streamlit as st
 from funcs.utils import download_and_process_csv, upload_dataframe_to_drive
@@ -25,22 +27,29 @@ st.header('טבלת נעדרים')
 
 st.session_state['uni_data'] = download_and_process_csv()
 
-# use the abot to create a tab  le columns
-if st.checkbox('הצג את כל העמודות'):
-    cols = st.session_state['uni_data'].columns
-else:
-    cols = ['שם', 'שם משפחה', 'ת.ז.', 'מין', 'גיל', 'אזרח/ית / חייל/ת', 'טלפון 1 של הנעדר/ת', 'שם המדווח/ת 1', 'טלפון של המדווח/ת 1']
+if not DB_ON:
+    # use the abot to create a tab  le columns
+    if st.checkbox('הצג את כל העמודות'):
+        cols = st.session_state['uni_data'].columns
+    else:
+        cols = ['שם', 'שם משפחה', 'ת.ז.', 'מין', 'גיל', 'אזרח/ית / חייל/ת', 'טלפון 1 של הנעדר/ת', 'שם המדווח/ת 1', 'טלפון של המדווח/ת 1']
 
-# write the table in the center of the page
-st.write(st.session_state['uni_data'][cols])
+    # write the table in the center of the page
+    st.write(st.session_state['uni_data'][cols])
 
 
-if st.button('save'):
-    upload_dataframe_to_drive(st.session_state['uni_data'])
-    st.write('הטבלה נשמרה בהצלחה')
+    if st.button('save'):
+        upload_dataframe_to_drive(st.session_state['uni_data'])
+        st.write('הטבלה נשמרה בהצלחה')
 
 # download csv button
 
 
 # st.line_chart(data)
+else:
+    conn = st.experimental_connection("postgresql", type="sql")
+    df = conn.query('SELECT * FROM mytable;', ttl="10m")
+    for row in df.itertuples():
+        st.write(f"{row.name} has a :{row.pet}:")
+
 
