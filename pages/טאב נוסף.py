db8069f3d93ae8a7b78d7de5c2397ad9c098b92d
ronnee19 <122403782+ronstip@users.noreply.github.com>
2023-10-11 09:41:47 +0300

    changed req2

--- pages/טאב נוסף.py
+++ pages/טאב נוסף.py
@@ -1,14 +1,16 @@
-# streamlit_app.py
+# # streamlit_app.py
 
-import streamlit as st
-from streamlit_gsheets import GSheetsConnection
+# import streamlit as st
+# from streamlit_gsheets import GSheetsConnection
 
-# Create a connection object.
-conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+# # Create a connection object.
+# conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-df = conn.read()
+# df = conn.read()
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
-    
+# # Print results.
+# for row in df.itertuples():
+#     st.write(f"{row.name} has a :{row.pet}:")
+
+# print all the installed packages with their versions
+print(pip freeze)
